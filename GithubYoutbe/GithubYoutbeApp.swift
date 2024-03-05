6fc80f9cd351a72c9b68ce8be12702dbb19b1ed1
Bruno Marques <bruno.marques@townsq.com.br>
2024-03-04 22:55:29 -0300

    primeiro commit

---
+++ GithubYoutbe/GithubYoutbeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubYoutbeApp.swift
+//  GithubYoutbe
+//
+//  Created by Bruno Marques on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubYoutbeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
